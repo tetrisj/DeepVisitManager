93f0b53d42ed419c5d5d5728eb9729ca7bd56006
jeniag <jeniag@similargroup.com>
2016-02-26 12:43:26 +0200

    - Added spark job to process data

--- input.py
+++ input.py
@@ -1,31 +1,61 @@
 import numpy as np
 from hashlib import md5
+import os
+import simplejson
 
 
 def url_digest(url_str):
     # TODO: This is stupid. Change to something meningful
+    if type(url_str) is unicode:
+        url_str = url_str.encode('utf-8')
+    if url_str.startswith('http://') or url_str.startswith('https://'):
+        url_str = url_str.split('://', 1)[-1]
+    if url_str.startswith('www.'):
+        url_str = url_str[4:]
+    url_str = url_str.rstrip('/')
     h = md5(url_str)
-    return float(int(h.hexdigest()[:8], 16))
+    return int(h.hexdigest()[:8], 16)
 
 
-def read_line(line_str):
-    fields = line_str.split('\t')
-    timestamp = float(fields[0])
-    user_id = fields[3]
-    referrer_url = fields[10]
-    prev_url = fields[11]
-    request_url = fields[12]
+def parse_events(event_str):
+    import simplejson
 
-    referrer_id = url_digest(referrer_url)
-    prev_id = url_digest(prev_url)
-    request_id = url_digest(request_url)
+    data = simplejson.loads(event_str.strip())
+    n = len(data['events'])
+    Y = np.zeros(n)
+    X = np.zeros((n, 4))
+    accepted = set([url_digest(url) for url in data['pages']])
+    for i, event in enumerate(data['events']):
+        timestamp = event['timestamp']
+        request_id = url_digest(event['requestUrl'])
+        referrer_id = url_digest(event['referrerUrl'])
+        prev_id = url_digest(event['prevUrl'])
+        X[i] = np.array([timestamp, request_id, referrer_id, prev_id])
+        if request_id not in accepted:
+            Y[i] = 2  # not mine
+    #TODO: separate domain from pages in id
+    #TODO: start timestamps from zero
+    return X, Y
 
-    desc = np.array([timestamp, request_id, referrer_id, prev_id])
-    return user_id, desc
+
+def event_data(root_path):
+    for filename in os.listdir(root_path):
+        if 'part' not in filename:
+            continue
+        file_path = os.path.join(root_path, filename)
+        for line in file(file_path, 'rb'):
+            yield parse_events(line)
+
+
+def rolling_window(a, window):
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 def main():
-    s= '1447088383.953	1560	chrome	0I3h95H8iKrsMDX	186665264191105950	106.44.112.145	156	Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36 QQBrowser/9.0.2315.400	0	0		http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&ch=2&tn=98010089_dg&wd=%E4%B8%80%E7%AB%99%E5%88%B0%E5%BA%952012&oq=%E4%B8%80%E7%AB%99%E5%88%B0%E5%BA%9520120%26lt%3B02%E6%9C%9F&rsv_pq=fcd25dbb00013555&rsv_t=53ceDrjgIY7YdPiQ0QULMX%2FgGVfrC4xXi%2FS9azzDzv%2FR5M2CtGHszSwhvqVHMQeOav8&rsv_enter=1&rsv_sug3=37&rsv_sug1=34&bs=%E4%B8%80%E7%AB%99%E5%88%B0%E5%BA%9520120302%E6%9C%9F	http://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&ch=2&tn=98010089_dg&wd=%E4%B8%80%E7%AB%99%E5%88%B0%E5%BA%95%E9%A2%98%E5%BA%93%E5%8F%8A%E7%AD%94%E6%A1%88&oq=%E4%B8%80%E7%AB%99%E5%88%B0%E5%BA%952012&rsv_pq=8968b96e00012abb&rsv_t=fad7eCTKdKtaOIwlnhiYEmijYs%2B7Jg4TeDAegLYQ%2Bk%2FNcMkzARd%2FyG6k8TwJpRtPhh8&rsv_enter=1&rsv_sug1=38&bs=%E4%B8%80%E7%AB%99%E5%88%B0%E5%BA%952012	0								4015		7899c873995bec6f0148e3447b564201'
-    print read_line(s)
+    one_input = event_data(r'C:\Projects\Deep\combined').next()
+    print one_input
+
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -6,9 +6,10 @@ from keras.layers.recurrent import LSTM
 import numpy as np
 
 # time_delta, target_domain_id, target_path_id, referer_domain_id, referer_path_id, prev_domain_id, prev_path_id
+from input import event_data
 
 states = ('open', 'moved', 'not-mine', 'closed')
-n_desc = 7
+n_desc = 4
 max_t = 20
 
 model = Sequential()
@@ -21,16 +22,18 @@ model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-### Feature structure
 
+### Feature structure
+# timestamp[float], request_id[float], referer_id[float], prev_id[float]
 
 
 def main():
-    X = np.ones((1, max_t, n_desc))
-    Y = np.ones((1, 1))
-    print model.predict(X, 128)
-    model.fit(X, Y)
-    print model.predict(X, 128)
+    for X, Y in event_data(r'C:\Projects\Deep\combined'):
+        print X[:max_t].reshape((1, max_t, n_desc))
+        print Y[:max_t].reshape(1, max_t)
+        print X,Y
+        model.fit(X, Y)
+        print model.predict(X, 128)
 
 
 if __name__ == '__main__':
